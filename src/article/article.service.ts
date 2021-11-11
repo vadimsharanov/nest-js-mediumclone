944dfc56d1edd6a9125ca6ed70235fbc3791b02d
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-11 11:57:13 +0200

    article dto added

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,8 +1,30 @@
+import { UserEntity } from "@app/user/user.entity";
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { ArticleEntity } from "./article.entity";
+import { CreateArticleDto } from "./dto/createArticle.dto";
 
 @Injectable()
 export class ArticleService {
-  async createArticle() {
-    return "service create article";
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>
+  ) {}
+  async createArticle(
+    currentUser: UserEntity,
+    createArticleDto: CreateArticleDto
+  ): Promise<ArticleEntity> {
+    const article = new ArticleEntity();
+    Object.assign(article, createArticleDto);
+    if (!article.tagList) {
+      article.tagList = [];
+    }
+    if (!article.slug) {
+      article.slug = "";
+    }
+
+    article.author = currentUser;
+    return await this.articleRepository.save(article);
   }
 }
